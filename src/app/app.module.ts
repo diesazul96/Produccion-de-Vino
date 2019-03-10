a506aaadba581be278ce7834b21ad9f71d1f213e
Santiago <santirogo@gmail.com>
2019-03-10 15:23:19 -0500

    Agregadas vistas 2, 4 y 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioSesionComponent } from '../inicioSesion/inicioSesion.component';
+import { MenuComponent } from '../menu/menu.component';
+import { NewParcelComponent } from '../newParcel/newParcel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, InicioSesionComponent, MenuComponent, NewParcelComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
     AppRoutingModule
   ],
