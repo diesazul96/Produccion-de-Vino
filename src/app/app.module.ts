97b909c1e7f03d341525bbab313c58e3867a018b
mauricionietom <30412495+mauricionietom@users.noreply.github.com>
2019-03-12 11:32:59 -0500

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { LoginComponent } from './login/login.component';
+import { ResterpasswordComponent } from './resterpassword/resterpassword.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, LoginComponent, ResterpasswordComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/resterpassword/resterpassword.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resterpassword',
+  templateUrl: './resterpassword.component.html',
+  styleUrls: ['./resterpassword.component.css']
+})
+export class ResterpasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
