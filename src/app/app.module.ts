6c18956d66f553b96f6d5a761cbfed2d4db80096
diesazul96 <diesazul96@hotmail.com>
2019-03-14 12:06:07 -0500

    Commit Diego

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,10 @@ import { RegistroComponent } from '../registro/registro.component';
 import { TratamientoComponent } from '../tratamiento/tratamiento.component';
 import { MovimientoComponent } from '../movimiento/movimiento.component';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
-import{ MaterialModule, MatNativeDateModule} from '@angular/material';
+import{ MatNativeDateModule} from '@angular/material';
 
 import{ MatDatepickerModule} from '@angular/material/datepicker';
-import{ MatButtonModule} from '@angular/material/button';
-import{ MatCardModule} from '@angular/material/card';
-import{ MatIconModule} from '@angular/material/icon';
 import{ MatInputModule} from '@angular/material/input';
-import{ MatTabsModule} from '@angular/material/tabs';
-import{ MatMomentDateModule} from '@angular/material-moment-adapter';
 
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -32,14 +27,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatDatepickerModule,
-    MatMomentDateModule,
     FormsModule,
     ReactiveFormsModule,
-    MatIconModule,
-    MatCardModule,
-    MatButtonModule,
     MatInputModule,
-    MatTabsModule,
     MatNativeDateModule
 
 
