f826ded3b46a7fe5a07fb55e6c937ab9aabb5085
Santiago <santirogo@gmail.com>
2019-04-17 10:44:37 -0500

    Routing añadido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,14 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InicioSesionComponent } from '../inicioSesion/inicioSesion.component';
-import { MenuComponent } from '../menu/menu.component';
-import { NewParcelComponent } from '../newParcel/newParcel.component';
 import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent, InicioSesionComponent, MenuComponent, NewParcelComponent
+    AppComponent, routingComponents
   ],
   imports: [
     NgbModule,
