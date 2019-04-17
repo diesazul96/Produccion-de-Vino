4e72dada98492b343ea29bbac4697bdb92d11efe
Santiago <santirogo@gmail.com>
2019-04-16 21:36:24 -0500

    Añadida conexión con firebase. se añaden parcelas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioSesionComponent } from '../inicioSesion/inicioSesion.component';
 import { MenuComponent } from '../menu/menu.component';
 import { NewParcelComponent } from '../newParcel/newParcel.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -15,9 +18,10 @@ import { NewParcelComponent } from '../newParcel/newParcel.component';
   imports: [
     NgbModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
-  providers: [],
+  providers: [AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
